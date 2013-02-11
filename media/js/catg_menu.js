0fc91e968114895bbaf6b78163b728826512a12d
Steven Bohm <info@steven-bohm.net>
2013-02-11 08:10:22 +0100

    We can now parsing ids

--- media/js/catg_menu.js
+++ media/js/catg_menu.js
@@ -13,95 +13,12 @@
         l_catg_id,
         l_obj_type_id;
 
-function clicked_item(p_item, p_catg, p_obj_type){
-     l_clicked = p_item;
-     l_catg_id = p_catg
-     l_obj_type_id = p_obj_type
-};
+
 
 $(document).on('click', '#catg_menu_btn', function(){
-
-    clicked_item($(this).attr('data_obj_id'),0,$(this).attr('data_type_id'));
     $("#obj_catg_menu").trigger("pagebeforecreate");
 })
 
-$(document).on('click', '.menu_obj_details', function(){
-    $("#obj_details").trigger("pagebeforecreate");
-})
-
-$(document).delegate("#obj_menu", "pagebeforecreate", function() {
-
-    var catg_id = l_clicked;
-    var aData = '{ "method": "cmdb.objects",' +
-        '"params": { ' +
-        '"session": {' +
-        '"username": "admin",' +
-        '"password": "21232f297a57a5a743894a0e4a801fc3",' +
-        '"language": "de",' +
-        '"mandator": 1},' +
-        '"filter": {' +
-        '"type": ' + catg_id +
-        '}' +
-        '},' +
-        '"jsonrpc": "2.0"' +
-        '}';
-    request(aData, function (json) {
-        l_catg_menu = json;
-        buildObjMenu(l_catg_menu,catg_id);
-    });
-});
-
-$(document).delegate('#obj_details', "pagebeforecreate", function(){
-    var obj_id = l_clicked;
-    var catg_id = l_catg_id;
-    var obj_type = l_obj_type_id;
-
-    $('#catg_menu_btn').attr('data_obj_id', l_clicked);
-    $('#catg_menu_btn').attr('data_type_id',l_obj_type_id);
-
-    var aData = '{ "method": "cmdb.category",' +
-        '"params": { ' +
-        '"session": {' +
-        '"username": "admin",' +
-        '"password": "21232f297a57a5a743894a0e4a801fc3",' +
-        '"language": "de",' +
-        '"mandator": 1},' +
-        '"objID":' + obj_id + ',' +
-        '"catgID": ' + catg_id +
-        '},' +
-        '"jsonrpc": "2.0"' +
-        '}';
-    request(aData, function (json) {
-        l_obj_detail_menu = json;
-        buildDetail(l_obj_detail_menu, obj_id, obj_type);
-    });
-
-});
-
-function getCatgInfo(){
-
-}
-
-$(document).delegate('#obj_catg_menu', "pagebeforecreate", function(){
-    var obj_id = l_clicked;
-    var obj_type_id = l_obj_type_id;
-    var aData = '{ "method": "cmdb.object_type_categories",' +
-        '"params": { ' +
-        '"session": {' +
-        '"username": "admin",' +
-        '"password": "21232f297a57a5a743894a0e4a801fc3",' +
-        '"language": "de",' +
-        '"mandator": 1},' +
-        '"type":' + obj_type_id + ',' +
-        '"order_by": "title"' +
-        '},' +
-        '"jsonrpc": "2.0"' +
-        '}';
-    request(aData, function (json) {
-        l_obj_catg_menu = json;
-        buildCatgMenu(l_obj_catg_menu, obj_id, obj_type_id);
-    });
-})
 
 
     function loadMenu() {
@@ -130,33 +47,6 @@ $(document).delegate('#obj_catg_menu', "pagebeforecreate", function(){
 
     }
 
-    function request(p_json, callback) {
-        $.mobile.loading('show', {
-            text:'Wird geladen',
-            textVisible:true,
-            theme:'b',
-            html:""
-        });
-        $.ajax({
-            type:'POST',
-            url:'http://steven-bohm.net/idoit-pro/index.php?api=jsonrpc',
-            data:p_json,
-            crossDomain:true,
-            headers:{'idoitapi':'request'},
-            //contentType:'application/json',
-            dataType:'json',
-            success:function (json) {
-                l_last_response = json;
-                callback(json);
-                $.mobile.loading('hide');
-                $.mobile.silentScroll();
-            },
-            error:function (json) {
-                l_last_response = json;
-            }
-
-        })
-    }
 
     function buildMenu(p_json_return) {
 
@@ -170,17 +60,17 @@ $(document).delegate('#obj_catg_menu', "pagebeforecreate", function(){
             if (p_json_return.result.hasOwnProperty(i)) {
                 item = p_json_return.result[i];
                 if (item.tree_group == 1) {
-                    list_tree_1.push('<li><a href="#obj_menu" onclick="javascript:clicked_item('+ item.id +')"  class="menu_catg">' + item.title + '</a></li>');
+                    list_tree_1.push('<li><a href="obj_menu.html#obj_menu?type_id='+item.id+'" data-ajax="false" rel="external"   class="menu_catg">' + item.title + '</a></li>');
                 }
                 if (item.tree_group == 2) {
-                    list_tree_2.push('<li><a href="#obj_menu" onclick="javascript:clicked_item('+ item.id +')"   class="menu_catg">' + item.title + '</a></li>');
+                    list_tree_2.push('<li><a href="obj_menu.html#obj_menu?type_id='+item.id+'" data-ajax="false" rel="external"    class="menu_catg">' + item.title + '</a></li>');
                 }
                 if (item.tree_group == 3) {
-                    list_tree_3.push('<li><a href="#obj_menu" onclick="javascript:clicked_item('+ item.id +')"   class="menu_catg">' + item.title + '</a></li>');
+                    list_tree_3.push('<li><a href="obj_menu.html#obj_menu?type_id='+item.id+'" data-ajax="false" rel="external"    class="menu_catg">' + item.title + '</a></li>');
                 }
 
                 if (item.tree_group == 1000) {
-                    list_tree_4.push('<li><a href="#obj_menu" onclick="javascript:clicked_item('+ item.id +')"  class="menu_catg">' + item.title + '</a></li>');
+                    list_tree_4.push('<li><a href="obj_menu.html#obj_menu?type_id='+item.id+'" data-ajax="false" rel="external"   class="menu_catg">' + item.title + '</a></li>');
                 }
 
             }
@@ -214,64 +104,14 @@ $(document).delegate('#obj_catg_menu', "pagebeforecreate", function(){
             '</div>');
 
         $('.menu_lst').listview();
-        $('.menu_tree').collapsible({inset: true});
+        $('.menu_tree').collapsible({inset: false});
 
     }
-
-function buildObjMenu(p_json_return, p_obj_type) {
-
-    var list = [],
-        item;
-
-    for (var i in p_json_return.result) {
-        if (p_json_return.result.hasOwnProperty(i)) {
-            item = p_json_return.result[i];
-            list.push('<li><a href="#obj_details" onclick="javascript:clicked_item('+ item.id +',1,'+ p_obj_type + ')" class="menu_obj_details">' + item.title + '</a></li>');
-
-        }
-
-    }
-
-    $('#objMenu_listitems').html('' +
-        '<ul data-role="listview" class="menu_lst" data-inset="true">' +
-        list.join("") +
-        '</ul>' +
-        '</div>');
-
-
-    $('.menu_lst').listview();
-}
 
 function buildDetail(p_json_return, p_obj_id, p_obj_type) {
            // $("input[id^=obj_name]:last").val(p_json_return.result[0].title);
 
 
-}
-
-function buildCatgMenu(p_json_return, p_obj_id, p_obj_type) {
-
-var list = [],
-        item,
-        item_temp,
-        s = 0;
-
-    for (var i in p_json_return.result) {
-    if (p_json_return.result.hasOwnProperty(i)) {
-        item_temp = p_json_return.result[i];
-      for (var s in item_temp){
-          item = item_temp[s];
-        list.push('<li><a href="#obj_details" onclick="javascript:clicked_item('+ p_obj_id +','+ item.id +','+ p_obj_type + ')" class="menu_obj_details">' + item.title + '</a></li>');
-    }
-    }
-   }
-
-    $('#obj_catg_listitems').html('' +
-        '<ul data-role="listview" class="menu_lst_panel" data-inset="true">' +
-        list.join("") +
-        '</ul>' +
-        '</div>');
-
-    $('.menu_lst_panel').listview();
 }
 
 $(document).on('swiperight', function(){
--- media/js/obj_menu.js
+++ media/js/obj_menu.js
@@ -1,65 +1,44 @@
 /**
  * Created with JetBrains PhpStorm.
  * User: sbohm
- * Date: 01.02.13
- * Time: 14:02
+ * Date: 08.02.13
+ * Time: 10:13
  * To change this template use File | Settings | File Templates.
  */
 
-    var l_last_response,
-        l_menu,
-        l_catg_menu,
-        l_obj_detail_menu;
+$(document).on('click', '.menu_obj_details', function(){
+    $("#obj_details").trigger("pagebeforecreate");
+})
 
-    function loadMenu() {
-            var catg_id = $(this).attr('data-catg');
-            var aData = '{ "method": "cmdb.objects",' +
-                '"params": { ' +
-                '"session": {' +
-                '"username": "admin",' +
-                '"password": "21232f297a57a5a743894a0e4a801fc3",' +
-                '"language": "de",' +
-                '"mandator": 1},' +
-                '"filter": {' +
-                '"type": ' + catg_id +
-                '}' +
-                '},' +
-                '"jsonrpc": "2.0"' +
-                '}';
-            request(aData, function (json) {
-                l_catg_menu = json;
-                buildCatgMenu(l_catg_menu,catg_id);
-            });
-    }
 
-    function request(p_json, callback) {
-        $.mobile.loading('show', {
-            text:'Wird geladen',
-            textVisible:true,
-            theme:'b',
-            html:""
-        });
-        $.ajax({
-            type:'POST',
-            url:'http://steven-bohm.net/idoit-pro/index.php?api=jsonrpc',
-            data:p_json,
-            crossDomain:true,
-            headers:{'idoitapi':'request'},
-            //contentType:'application/json',
-            dataType:'json',
-            success:function (json) {
-                l_last_response = json;
-                callback(json);
-                $.mobile.loading('hide');
-                $.mobile.silentScroll();
-            },
-            error:function (json) {
-                l_last_response = json;
-            }
+$(document).bind("pagebeforechange", function( event, data ) {
+    $.mobile.pageData = (data && data.options && data.options.pageData)
+        ? data.options.pageData
+        : null;
 
-        })
-    }
-function buildCatgMenu(p_json_return, p_obj_type) {
+    var catg_id = $.mobile.pageData.type_id;
+    var aData = '{ "method": "cmdb.objects",' +
+        '"params": { ' +
+        '"session": {' +
+        '"username": "admin",' +
+        '"password": "21232f297a57a5a743894a0e4a801fc3",' +
+        '"language": "de",' +
+        '"mandator": 1},' +
+        '"filter": {' +
+        '"type": ' + catg_id +
+        '}' +
+        '},' +
+        '"jsonrpc": "2.0"' +
+        '}';
+    request(aData, function (json) {
+        l_catg_menu = json;
+        buildObjMenu(l_catg_menu,catg_id);
+    });
+});
+
+
+
+function buildObjMenu(p_json_return, p_obj_type) {
 
     var list = [],
         item;
@@ -67,14 +46,14 @@ function buildCatgMenu(p_json_return, p_obj_type) {
     for (var i in p_json_return.result) {
         if (p_json_return.result.hasOwnProperty(i)) {
             item = p_json_return.result[i];
-            list.push('<li><a href="#" data_obj_type = "'+ p_obj_type +'" data-obj_catg_id="1" data-obj_id="' + item.id + '" class="menu_obj_details">' + item.title + '</a></li>');
+            list.push('<li><a href="details.html#obj_details?obj_id='+item.id+'&typ_id='+p_obj_type+'&catg_id=1" data-ajax="false" rel="external class="menu_obj_details">' + item.title + '</a></li>');
 
         }
 
     }
 
-    $('#objMenu').html('' +
-        '<ul data-role="listview" class="menu_lst">' +
+    $('#objMenu_listitems').html('' +
+        '<ul data-role="listview" class="menu_lst" data-inset="false">' +
         list.join("") +
         '</ul>' +
         '</div>');
