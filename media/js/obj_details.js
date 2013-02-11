19a63468ff7d9f2630f6b95c096e3f4881cc4634
Steven Bohm <info@steven-bohm.net>
2013-02-11 08:15:00 +0100

    We can now parsing ids

---
+++ media/js/obj_details.js
@@ -0,0 +1,47 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: sbohm
+ * Date: 08.02.13
+ * Time: 10:13
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).on('click', '#backButton', function(){
+    $("#obj_menu").trigger("pagebeforecreate");
+})
+
+$(document).bind("pagebeforechange", function( event, data ) {
+    $.mobile.pageData = (data && data.options && data.options.pageData)
+        ? data.options.pageData
+        : null;
+
+    var catg_id = $.mobile.pageData.catg_id;
+    var obj_id = $.mobile.pageData.obj_id;
+    var obj_type = $.mobile.pageData.typ_id;
+
+    var aData = '{ "method": "cmdb.category",' +
+        '"params": { ' +
+        '"session": {' +
+        '"username": "admin",' +
+        '"password": "21232f297a57a5a743894a0e4a801fc3",' +
+        '"language": "de",' +
+        '"mandator": 1},' +
+        '"objID":' + obj_id + ',' +
+        '"catgID": ' + catg_id +
+        '},' +
+        '"jsonrpc": "2.0"' +
+        '}';
+    request(aData, function (json) {
+        l_obj_detail_menu = json;
+        $('#headTitle').html(json.title);
+        $('#backButton').attr('href', 'obj_menu.html#obj_menu?type_id='+obj_type);
+        buildDetail(l_obj_detail_menu, obj_id, obj_type);
+    });
+});
+
+
+function buildDetail(p_json_return, p_obj_id, p_obj_type) {
+    // $("input[id^=obj_name]:last").val(p_json_return.result[0].title);
+
+
+}
---
+++ media/js/request.js
@@ -0,0 +1,35 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: sbohm
+ * Date: 08.02.13
+ * Time: 10:04
+ * To change this template use File | Settings | File Templates.
+ */
+
+function request(p_json, callback) {
+    $.mobile.loading('show', {
+        text:'Wird geladen',
+        textVisible:true,
+        theme:'b',
+        html:""
+    });
+    $.ajax({
+        type:'POST',
+        url:'http://steven-bohm.net/idoit-pro/index.php?api=jsonrpc',
+        data:p_json,
+        crossDomain:true,
+        headers:{'idoitapi':'request'},
+        //contentType:'application/json',
+        dataType:'json',
+        success:function (json) {
+            l_last_response = json;
+            callback(json);
+            $.mobile.loading('hide');
+            $.mobile.silentScroll();
+        },
+        error:function (json) {
+            l_last_response = json;
+        }
+
+    })
+}
