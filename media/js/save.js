c0104b763ae2d0cac38002b1e4ce7b4e795f8d4c
Steven Bohm <info@steven-bohm.net>
2013-02-11 10:19:02 +0100

    Config Test

---
+++ media/js/save.js
@@ -0,0 +1,21 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: sbohm
+ * Date: 11.02.13
+ * Time: 10:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).on('click','#saveConfig', function(){
+    window.localStorage.setItem("hostname", $('#hostname').val());
+    window.localStorage.setItem("apikey", $('#apikey').val());
+    window.localStorage.setItem("language", $('#lang').val());
+    alert('Gespeichert'+ window.localStorage.getItem('hostname'));
+})
+
+$(document).ready(function(){
+    $('hostname').val(window.localStorage.getItem('hostname'));
+    $('apikey').val(window.localStorage.getItem('apikey'));
+    $('lang').val(window.localStorage.getItem('language'));
+})
+
